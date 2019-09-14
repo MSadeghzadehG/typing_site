d6f3c0b986c6b1c78c068f5f3c69e0fad6539841
9631044 <9631044@ceit.local>
2019-09-14 06:12:33 +0430

    feat: add Test,TestText, Record models. fix secret key problem

--- typing_site/test_provider/models.py
+++ typing_site/test_provider/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AbstractUsercd
+from django.contrib.auth.models import AbstractUser
+
+
+class Test(models.Model):
+    text = models.ForeignKey("TestText", on_delete=models.CASCADE)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    record = models.ForeignKey(
+        "Record", on_delete=models.CASCADE, null=True, default=None
+    )
+
+
+class TestText(models.Model):
+    text = models.TextField(unique=True, blank=False)
+
+
+class Record(models.Model):
+    speed = models.FloatField(null=False)
+    wrong_keys = models.IntegerField(null=False)
+    correct_keys = models.IntegerField(null=False)
+    time = models.FloatField(null=False)
 
 
 class User(AbstractUser):
